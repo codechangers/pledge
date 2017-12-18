ef1ddbcb559307ae9d62a8b27676b76b2114c797
Andrew <andrew@dsh-stem-2.local>
2017-12-18 12:17:17 -0700

    Database auto-migration

--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const db = {};
 
 let sequelize = null;
 
-if (process.env.DB_URL) {
+if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DB_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
